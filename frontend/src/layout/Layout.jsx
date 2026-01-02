c321ad158bcca26215fad03cadc9f16a28b3427b
trailtony <still_running_uphill@protonmail.com>
2026-01-02 22:50:38 +0000

    Added visible navigation bar when user logs in

--- frontend/src/layout/Layout.jsx
+++ frontend/src/layout/Layout.jsx
@@ -1,5 +1,29 @@
 import "react"
+import { SignedIn, SignedOut, UserButton } from "@clerk/clerk-react"
+import { Outlet, Link, Navigate } from "react-router-dom"
 
 export function Layout() {
-    return <></>
+    return <div className="app-layout">
+        <header className="app-header">
+            <div className="header-content">
+                <h1>Code Challenge generator</h1>
+                <nav>
+                    <SignedIn>
+                        <Link to="/">Generate Challenge</Link>
+                        <Link to="/history">History</Link>
+                        <UserButton/>
+                    </SignedIn>
+                </nav>
+            </div>
+        </header>
+        
+        <main className="app-main">
+            <SignedOut>
+                <Navigate to="/sign-in" replace/>
+            </SignedOut>
+            <SignedIn>
+                <Outlet />
+            </SignedIn>
+        </main>
+    </div>
 }
