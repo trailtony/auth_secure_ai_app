1a65e94d5bbc825dfab1c12a169679909c836ea3
trailtony <still_running_uphill@protonmail.com>
2026-01-06 16:44:08 +0000

    Added backend file structure and fastapi app client

---
+++ backend/src/app.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI, Request, Response
+from fastapi.middleware.cors import CORSMiddleware
+from clerk_backedn_api import Clerk
+import os
+
+clerk_sdk = Clerk(bearer_auth=os.getenv("CLERK_SECRET_KEY"))
+
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware, 
+    allow_origins=["*"], 
+    allow_credentials=True, 
+    allow_methods=["*"], 
+    allow_headers=["*"]
+    )
