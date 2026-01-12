c43e70618f433be1b14e9f755a62002ca45ccfe8
trailtony <still_running_uphill@protonmail.com>
2026-01-12 17:35:26 +0000

    Added challenge generation by LLM integration

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,9 +1,6 @@
 from fastapi import FastAPI, Request, Response
 from fastapi.middleware.cors import CORSMiddleware
-from clerk_backedn_api import Clerk
-import os
-
-clerk_sdk = Clerk(bearer_auth=os.getenv("CLERK_SECRET_KEY"))
+from .routes import challenge
 
 
 app = FastAPI()
@@ -15,3 +12,5 @@ app.add_middleware(
     allow_methods=["*"], 
     allow_headers=["*"]
     )
+
+app.include_router(challenge.router, prefix="/api")
