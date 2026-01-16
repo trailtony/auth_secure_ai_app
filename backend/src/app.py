3627d5b4425592c68a1a64527773a6fea70f0797
trailtony <still_running_uphill@protonmail.com>
2026-01-16 16:40:16 +0000

    Added Clerk webhooks to log user creation events

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,6 +1,6 @@
-from fastapi import FastAPI, Request, Response
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from .routes import challenge
+from .routes import challenge, webhooks
 
 
 app = FastAPI()
@@ -14,3 +14,4 @@ app.add_middleware(
     )
 
 app.include_router(challenge.router, prefix="/api")
+app.include_router(webhooks.router, prefix="/webhooks")
